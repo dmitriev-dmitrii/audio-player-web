bb03fe83ce8fab04f02354a2f4c645b4b2452345
Dmitriev-Dmitrii <dmitriev.dmitrii@mail.com>
2022-09-07 01:03:01 +0300

    test-submodules

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+
+<template>
+  <div>
+    <h1> h1
+    </h1>
+    <app-player />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+<script>
+import appPlayer from './components/appPlayer.vue';
+export default {
+  components :{ appPlayer },
+  data:()=> { return {
+
+  }
+  },
+
+}
+</script>
+
---
+++ src/components/appPlayer.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="track-list">
+    <audio controls="true"
+      src="https://cachev2-spb02.cdn.yandex.net/download.cdn.yandex.net/tech/ru/audio/jsapi/doc/examples/files/audio_src/Tchaikovsky1.mp3?lid=21">
+    </audio>
+  </div>
+</template>
+
+<style >
+
+</style>
